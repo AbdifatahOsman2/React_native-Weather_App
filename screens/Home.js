375a47114cbdbcee754a4e7b1e80bd2069bc6116
AbdifatahOsman2 <abdifatahosman205@gmail.com>
2022-01-28 00:02:07 -0700

    pulled all data and got city

--- screens/Home.js
+++ screens/Home.js
@@ -1,50 +1,70 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
-import * as Location from 'expo-location';
-import axios from 'axios';
+import { StyleSheet, Text, View, Image } from "react-native";
+import * as Location from "expo-location";
+import wind from "../assets/wind-solid.svg";
+import axios from "axios";
 
 export default function Home() {
-  const [long, setLong ] = useState(null);
+  const [long, setLong] = useState(null);
   const [lat, setLat] = useState(null);
   const [temp, settemp] = useState(null);
   const [min, setmin] = useState(null);
   const [max, setmax] = useState(null);
+  const [wind, setwind] = useState(null);
   const [humidity, sethumidity] = useState(null);
   const [description, setdescription] = useState(null);
-
+  const [city, setcity] = useState(null);
 
   useEffect(() => {
     (async () => {
       let { status } = await Location.requestForegroundPermissionsAsync();
-      if (status !== 'granted') {
-        console.error('Permission to access location was denied');
+      if (status !== "granted") {
+        console.error("Permission to access location was denied");
         return;
       }
-        let location = await Location.getCurrentPositionAsync({});
-        setLat(location.coords.latitude)
-        setLong(location.coords.longitude)
+      let location = await Location.getCurrentPositionAsync({});
+      setLat(location.coords.latitude);
+      setLong(location.coords.longitude);
     })();
   }, []);
 
-  async function getData () {
-    const response = await axios.get(`http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=3616a57498522c3f1df43d6caaa2c964&units=imperial`)
+  async function getData() {
+    const response = await axios.get(
+      `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=3616a57498522c3f1df43d6caaa2c964&units=imperial`
+    );
     console.log(response.data)
-    setdescription(response.data.weather[0].description)
-    settemp(response.data.main.temp)
-    setmin(response.data.main.temp_min)
-    setmax(response.data.main.temp_max)
-    sethumidity(response.data.main.humidity)
+    setdescription(response.data.weather[0].description);
+    settemp(parseInt(response.data.main.temp));
+    setmin(parseInt(response.data.main.temp_min));
+    setmax(parseInt(response.data.main.temp_max));
+    sethumidity(response.data.main.humidity);
+    setwind(response.data.wind.speed);
   }
-  getData()
-  console.log()
+  getData();
+
+  async function getCity() {
+    const res = await axios.get(
+      `http://api.openweathermap.org/geo/1.0/reverse?lat=${lat}&lon=${long}&appid=3616a57498522c3f1df43d6caaa2c964`
+    );
+    setcity(res.data[0].local_names.en);
+  }
+  getCity();
 
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>
-        Home22
-      </Text>
-      <Text style={styles.text}>{temp}</Text>
+      <Text style={styles.city}>{city}</Text>
+      <View>
+      <Text style={styles.des}>{description}</Text>
+      <Text style={styles.temp}>{temp}°</Text>
+      </View>
+      <View>
+      <Text style={styles.bot}>Wind speed: {wind}</Text>
+      <Text style={styles.bot}>Humidity: {humidity}</Text>
+      <Text style={styles.bot}>Max temperature: {max}°</Text>
+      <Text style={styles.bot}>Min temperature: {min}°</Text>
+      </View>
+
       <StatusBar style="auto" />
     </View>
   );
@@ -56,11 +76,32 @@ const styles = StyleSheet.create({
     backgroundColor: "#1d2026",
     alignItems: "center",
     justifyContent: "center",
-    width: 500
+    justifyContent:'space-around',
+    width: 500,
   },
-  text: {
+  temp: {
     color: "white",
-    fontSize: 23,
+    fontSize: 180,
+    textAlign: "left",
+    fontFamily: "Arial",
+  },
+  des: {
+    color: "white",
+    fontSize: 50,
+    textAlign: "center",
+    marginRight: 30,
+    fontWeight: "200",
+  },
+  bot: {
+    color: "white",
+    margin:10,
+    fontSize: 25,
+    textAlign: "center",
+    fontStyle: "italic",
+  },
+  city: {
+    color: "white",
+    fontSize: 30,
     textAlign: "center",
   },
 });
